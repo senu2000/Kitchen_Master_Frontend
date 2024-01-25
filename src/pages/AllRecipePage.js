79af3c59d79b71df02411da7d99a56418efad81e
Raveen Chandima <cst20080@std.uwu.ac.lk>
2024-01-25 21:43:36 +0530

    edit recipe , view recipe

--- src/pages/AllRecipePage.js
+++ src/pages/AllRecipePage.js
@@ -1,173 +1,169 @@
-import React from 'react';
+import React from "react";
 import Navbardrk from "../components/Navbar";
 import AllRcard from "../components/AllRecipeCard";
 import Footerdark from "../components/Footer";
 import "../css/AllRecipecss.css";
 
 function AllRecipePage(props) {
-    return (
-        <div className="all-recipe-page">
-            <Navbardrk/>
-            <div id="meals">
-            <h2>Breakfast Meals</h2>
-            <hr id="hr1"/>
-            <div id = "r-card-area" className="flex">
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-            </div>
+  return (
+    <div className="all-recipe-page">
+      <Navbardrk />
+      <div id="meals">
+        <h2>Breakfast Meals</h2>
+        <hr id="hr1" />
+        <div id="r-card-area" className="flex">
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
         </div>
+      </div>
 
-        <div id="meals">
-            <h2>Lunch Meals</h2>
-            <hr id="hr1"/>
-            <div id = "r-card-area" className="flex">
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-            </div>
+      <div id="meals">
+        <h2>Lunch Meals</h2>
+        <hr id="hr1" />
+        <div id="r-card-area" className="flex">
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
         </div>
+      </div>
 
-        <div id="meals">
-            <h2>Dinner Meals</h2>
-            <hr id="hr1"/>
-            <div id = "r-card-area" className="flex">
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-            </div>
+      <div id="meals">
+        <h2>Dinner Meals</h2>
+        <hr id="hr1" />
+        <div id="r-card-area" className="flex">
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
         </div>
+      </div>
 
-        <div id="meals">
-            <h2>Quick Meals</h2>
-            <hr id="hr1"/>
-            <div id = "r-card-area" className="flex">
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-            </div>
+      <div id="meals">
+        <h2>Quick Meals</h2>
+        <hr id="hr1" />
+        <div id="r-card-area" className="flex">
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
         </div>
+      </div>
 
-        <div id="meals">
-            <h2>Vege Meals</h2>
-            <hr id="hr1"/>
-            <div id = "r-card-area" className="flex">
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-            </div>
+      <div id="meals">
+        <h2>Vege Meals</h2>
+        <hr id="hr1" />
+        <div id="r-card-area" className="flex">
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
         </div>
+      </div>
 
-        <div id="meals">
-            <h2>Drinks</h2>
-            <hr id="hr1"/>
-            <div id = "r-card-area" className="flex">
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-            </div>
+      <div id="meals">
+        <h2>Drinks</h2>
+        <hr id="hr1" />
+        <div id="r-card-area" className="flex">
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
         </div>
+      </div>
 
-        <div id="meals">
-            <h2>Deserts</h2>
-            <hr id="hr1"/>
-            <div id = "r-card-area" className="flex">
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-                <div id = "r-card">
-                    <AllRcard/>
-                </div>
-            </div>
+      <div id="meals">
+        <h2>Deserts</h2>
+        <hr id="hr1" />
+        <div id="r-card-area" className="flex">
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
+          <div id="r-card">
+            <AllRcard />
+          </div>
         </div>
-        <Footerdark/>
-        
-        </div>
-        
-
-
-    );
+      </div>
+      <Footerdark />
+    </div>
+  );
 }
 
 export default AllRecipePage;
